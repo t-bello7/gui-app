954f4dddbe2e63db8520671a132d217a71ad837a
tbello <bello4aus@gmail.com>
2023-04-07 02:35:06 +0100

    - add camera functionality (take picture and save image)

--- app/pages/Scan.tsx
+++ app/pages/Scan.tsx
@@ -1,38 +1,70 @@
-import React, {Fragment, useEffect} from 'react';
-import { Text, TouchableOpacity, Dimensions, Image} from 'react-native';
+import React, { useEffect } from 'react';
+import { Text, StyleSheet, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import ImagePicker from 'react-native-image-crop-picker'
-
-
+import { RNCamera } from 'react-native-camera'
+import { useCamera } from 'react-native-camera-hooks';
+import RNFS from 'react-native-fs';
+import { err } from 'react-native-svg/lib/typescript/xml';
 
 const Scan = () => {
 
-    const openVideo = () => {
-        ImagePicker.openCamera({
-            mediaType: 'video',
-        }).then( imageRes => {
-            console.log(imageRes)
+    const [ {cameraRef}, {takePicture}] = useCamera(null)
+
+    const captureImage = async () => {
+      try {
+        const {uri: imagePath} = await takePicture()
+        console.log(imagePath)
+        const newImagePath = RNFS.ExternalDirectoryPath + '/mytest.jpg'
+        RNFS.moveFile(imagePath, newImagePath).then(() => {
+          console.log(`image file was moved from ${imagePath} to ${newImagePath}`)
+        }).catch(err => {
+          console.log(err);
         })
+      }
+      catch (err) {
+        console.log(err)
+      }
     }
-
     useEffect(() => {
-        openVideo()
-
-        return () => {
-            ImagePicker.clean().then(() => {
-                console.log('removed all tmp images from tmp directory');
-            })
-        }
     },[])
 
-
     return (
-        <Fragment>
-            <SafeAreaView>
-                <Text>Directly Launch Camera</Text>
-            </SafeAreaView>
-        </Fragment>
-    )   
+        <SafeAreaView style={styles.body}>
+            <RNCamera
+                ref={cameraRef}
+                type={RNCamera.Constants.Type.back}
+                style={styles.preview}
+            >
+            <Button
+              title="Capture image"
+              onPress={captureImage}
+             />
+            </RNCamera>
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    body: {
+      flex: 1
+    },
+    container: {
+      display: "flex",
+      flexDirection: "column",
+      color: "black",
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    headline: {
+      fontWeight: '700',
+      fontSize: 24,
+      textAlign: 'center'
+    },
+    preview: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center"
+    }
+  })
 
 export default Scan;

382ed25ba0ddbc675454773ff21057d9ff2922b7
tbello <bello4aus@gmail.com>
2023-06-22 11:50:51 +0100

    - add bottomsheet component

--- app/components/BottomSheet.tsx
+++ app/components/BottomSheet.tsx
@@ -1,23 +1,24 @@
 import { useCallback, useContext, forwardRef, useImperativeHandle } from 'react';
 import {
-        StyleSheet,
-        Text,
-        View,
-        useWindowDimensions,
-        TouchableWithoutFeedback
-    } from 'react-native';
-import { ThemeContext } from '../../App';
+    StyleSheet,
+    Text,
+    View,
+    useWindowDimensions,
+    TouchableWithoutFeedback
+} from 'react-native';
 import Animated, {
     useSharedValue,
     useAnimatedStyle,
-    withSpring
-} from 'react-native-reanimated'
+    withSpring,
+    interpolate,
+} from 'react-native-reanimated';
+import { ThemeContext } from '../../App';
 
 const BottomSheet = forwardRef(({activeHeight}: any, ref) => {
     const theme = useContext(ThemeContext);
     const height = useWindowDimensions().height;
     const topAnimation = useSharedValue(height);
-    const newActveHeight = height - activeHeight;
+    const newActiveHeight = height - activeHeight;
     const styles = getStyles(theme)
     const animationStyle = useAnimatedStyle(() => {
         const top = topAnimation.value
@@ -25,9 +26,21 @@ const BottomSheet = forwardRef(({activeHeight}: any, ref) => {
             top,
         }
     })
+    const backDropAnimation = useAnimatedStyle(() => {
+        const opacity = interpolate(
+            topAnimation.value,
+            [height, newActiveHeight],
+            [0, 0.5]
+        )
+        const display = opacity === 0 ? 'none': 'flex';
+        return {
+            opacity, display
+        }
+    })
+
     const expand = useCallback(() => {
         'worklet';
-        topAnimation.value = withSpring(newActveHeight, {
+        topAnimation.value = withSpring(newActiveHeight, {
             damping: 100,
             stiffness: 400,
         });
@@ -35,20 +48,29 @@ const BottomSheet = forwardRef(({activeHeight}: any, ref) => {
     const close = useCallback(() => {
         'worklet';
         topAnimation.value = withSpring(height, {
-            damping: 100,
-
+         damping: 100,
         })
     }, [])
 
+    
     useImperativeHandle( ref, ()=> ({
         expand, close
     }), [expand, close])
     return (
-    <Animated.View style={[styles.container, animationStyle]}>
-        <View style={styles.lineContainer}>
-            <View style={styles.line} />
-        </View>
-    </Animated.View>
+    <>
+    <TouchableWithoutFeedback onPress={() => {
+        close();
+    }}>
+        <Animated.View style={[styles.backDrop, backDropAnimation]}/>
+    </TouchableWithoutFeedback>
+    {/* <PanGestureHandler onGestureEvent={gestureHandler}> */}
+        <Animated.View style={[styles.container, animationStyle]}>
+            <View style={styles.lineContainer}>
+                <View style={styles.line} />
+            </View>
+        </Animated.View>
+    {/* </PanGestureHandler> */}
+    </>
     )
 })
 
@@ -72,6 +94,14 @@ const getStyles = (theme: any) => StyleSheet.create({
         height: 4,
         backgroundColor: 'black',
         borderRadius: 10,
+    },
+    backDrop: {
+        backgroundColor: 'black',
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0
     }
 })
 export default BottomSheet;
